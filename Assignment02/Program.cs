8c8383bde6aed4c90cef848ea672daafe98f1cad
ayman <aymanehab2002@gmail.com>
2025-02-02 15:37:26 +0200

    Q02

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -18,16 +18,37 @@ namespace Assignment02
         } 
         #endregion
 
+        public static bool IsPalindrome(int [] arr)
+        {
+            int left = 0, right = arr.Length-1;
+            while (left < right)
+            {
+                if(arr[left] != arr[right])
+                    return false;
+                left++;
+                right--;
+
+
+
+            }
+            return true;
+        }
+
 
         static void Main(string[] args)
         {
             #region Q01
             //List<int> Numbers = new List<int>() { 11, 5, 3 };
-            
+
             //Console.WriteLine($"Numbers is grreater than num in list is {IsGreater(Numbers,1)}");
             //Console.WriteLine($"Numbers is grreater than num in list is {IsGreater(Numbers,7)}");
 
-            
+
+            #endregion
+
+            #region Q02
+            int[] Numbers = { 1, 3, 2, 3, 1 };
+            Console.WriteLine(IsPalindrome(Numbers));
             #endregion
         }
     }
