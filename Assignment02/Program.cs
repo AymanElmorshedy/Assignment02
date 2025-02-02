f620284edde7011c7f865f1612ef7a9deba90465
ayman <aymanehab2002@gmail.com>
2025-02-02 15:22:10 +0200

    Q01

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Assignment02
+﻿using System.Collections;
+
+namespace Assignment02
 {
     internal class Program
     {
+        #region Q01
+        public static int IsGreater(List<int> arr, int num)
+        {
+            int count = 0;
+            foreach (int i in arr)
+            {
+                if (i > num)
+                    count++;
+            }
+            return count;
+
+        } 
+        #endregion
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            //List<int> Numbers = new List<int>() { 11, 5, 3 };
+            
+            //Console.WriteLine($"Numbers is grreater than num in list is {IsGreater(Numbers,1)}");
+            //Console.WriteLine($"Numbers is grreater than num in list is {IsGreater(Numbers,7)}");
+
+            
+            #endregion
         }
     }
 }
